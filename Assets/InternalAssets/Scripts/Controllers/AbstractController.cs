1cd75dda53de2f2c3b08a89c9d02672d4d962edc
Sergey <grevich13@gmail.com>
2023-09-25 01:06:41 +0300

    Сontroller abstraction

---
+++ Assets/InternalAssets/Scripts/Controllers/AbstractController.cs
@@ -0,0 +1,70 @@
+using Task.Interfaces;
+using UnityEngine;
+namespace Task.Controllers
+{
+    public abstract class AbstractController : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _character;
+        [SerializeField]
+        private GameObject _weaponObject;
+        [SerializeField]
+        private GameObject _inventoryObject;
+        [SerializeField]
+        private GameObject _inventoryUIControllerObject;
+        private IInventoryUI _inventoryUIController;
+        private IInventory _inventory;
+        private IMovement _movement;
+        private IWeapon _weapon;
+        private bool isInventoryOpen = false;
+        void Start()
+        {
+            if (!_character.TryGetComponent(out _movement))
+            {
+                Debug.LogError("Movement logic not found", this);
+                throw new System.Exception("Movement logic not found");
+            }
+            if (!_weaponObject.TryGetComponent(out _weapon))
+            {
+                Debug.LogError("Weapon not found", this);
+                throw new System.Exception("Weapon not found");
+            }
+            if (!_inventoryObject.TryGetComponent(out _inventory))
+            {
+                Debug.LogError("Inventory not found", this);
+                throw new System.Exception("Inventory not found");
+            }
+            if (!_inventoryUIControllerObject.TryGetComponent(out _inventoryUIController))
+            {
+                Debug.LogError("InventoryUIController not found", this);
+                throw new System.Exception("InventoryUIController not found");
+            }
+        }
+        private void Update()
+        {
+            var vectorVelocity = MoveController();
+            _movement.SetVelocity(vectorVelocity.x, vectorVelocity.y);
+            if (IsShoot())
+                _weapon.Shoot();
+            if (IsPickUp())
+                _inventory.PickUp();
+            if (IsInventory())
+            {
+                if (isInventoryOpen)
+                {
+                    _inventoryUIController.CloseInventory();
+                    isInventoryOpen = false;
+                }
+                else
+                {
+                    _inventoryUIController.OpenInventory();
+                    isInventoryOpen = true;
+                }
+            }
+        }
+        public abstract Vector2 MoveController();
+        public abstract bool IsShoot();
+        public abstract bool IsPickUp();
+        public abstract bool IsInventory();
+    }
+}
--- Assets/InternalAssets/Scripts/Controllers/InputController.cs
+++ Assets/InternalAssets/Scripts/Controllers/InputController.cs
@@ -3,70 +3,28 @@ using UnityEditor.VersionControl;
 using UnityEngine;
 namespace Task.Controllers
 {
-    public class InputController : MonoBehaviour
+    public class InputController : AbstractController
     {
-        [SerializeField]
-        private GameObject _character;
-        [SerializeField]
-        private GameObject _weaponObject;
-        [SerializeField]
-        private GameObject _inventoryObject;
-        [SerializeField]
-        private GameObject _inventoryUIControllerObject;
-        private IInventoryUI _inventoryUIController;
-        private IInventory _inventory;
-        private IMovement _movement;
-        private IWeapon _weapon;
-        private bool isInventoryOpen = false;
-        void Start()
+        public override bool IsInventory()
         {
-            if (!_character.TryGetComponent(out _movement))
-            {
-                Debug.LogError("Movement logic not found", this);
-                throw new System.Exception("Movement logic not found");
-            }
-            if(!_weaponObject.TryGetComponent(out _weapon))
-            {
-                Debug.LogError("Weapon not found", this);
-                throw new System.Exception("Weapon not found");
-            }
-            if(!_inventoryObject.TryGetComponent(out _inventory))
-            {
-                Debug.LogError("Inventory not found", this);
-                throw new System.Exception("Inventory not found");
-            }
-            if(!_inventoryUIControllerObject.TryGetComponent(out _inventoryUIController))
-            {
-                Debug.LogError("InventoryUIController not found", this);
-                throw new System.Exception("InventoryUIController not found");
-            }
+            return Input.GetKeyDown(KeyCode.I);
         }
-        void Update()
+
+        public override bool IsPickUp()
+        {
+            return Input.GetKey(KeyCode.E);
+        }
+
+        public override bool IsShoot()
+        {
+            return Input.GetKey(KeyCode.Mouse0);
+        }
+
+        public override Vector2 MoveController()
         {
             float horizontal = Input.GetAxis("Horizontal");
             float vertical = Input.GetAxis("Vertical");
-            _movement.SetVelocity(horizontal, vertical);
-            if(Input.GetKey(KeyCode.Mouse0))
-            {
-                _weapon.Shoot();
-            }
-            if(Input.GetKey(KeyCode.E))
-            {
-                _inventory.PickUp();
-            }
-            if(Input.GetKeyDown(KeyCode.I))
-            {
-                if (isInventoryOpen)
-                {
-                    _inventoryUIController.CloseInventory();
-                    isInventoryOpen = false;
-                }
-                else
-                {
-                    _inventoryUIController.OpenInventory();
-                    isInventoryOpen = true;
-                }
-            }
+            return new Vector2(horizontal, vertical);
         }
     }
 }
